e8897f561915f7c48a69a0f1d1fb54e25cdc6b6e
Marguro <jarkkeryoki@gmail.com>
2025-05-18 10:29:45 +0700

    update : del for new repo

--- Assets/Scripts/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="NewItemData",menuName ="Workshop/ItemData")]
-public class ItemData : ScriptableObject
-{
-    public string itemName;
-    public string description;
-    public Sprite itemIcon;
-}
